e373c31ab8dbbc213feafeeb6e68f96b3fd7cd41
dmoulart <dorian@resalocal.com>
2022-03-15 18:54:04 +0100

    Add paginator

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -11,31 +11,55 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 class VehicleController extends MerosController
 {
 
     private VehicleRepository $repository;
+    private PaginatorInterface $paginator;
 
     function __construct(VehicleRepository $repository,
                          EntityManagerInterface $em,
-                         ValidatorInterface $validator){
+                         ValidatorInterface $validator,
+                         PaginatorInterface $paginator){
         parent::__construct($em, $validator);
         $this->repository = $repository;
+        $this->paginator = $paginator;
     }
 
     /**
-     * @Route("/vehicles/{id}", name="app_vehicles_get", methods={"GET"})
+     * @Route("/vehicles", name="app_vehicles_get", methods={"GET"})
      */
-    function find(int|string|null $id = null): Response
+    function findSome(Request $request): Response
     {
-        $vehicles = $this->repository->findOneOrAll($id);
+        $vehicles = $this->repository->findAll();
 
-        if(!$vehicles) return $this->json(
-             $id ? 'Cannot find vehicle with this id' : 'Cannot find vehicles'
-            ,404
+        $page = $request->get('page') ?? 1;
+
+        if(!$vehicles) return $this->json('Cannot find bookings', 404);
+
+        $response = $this->paginator->paginate(
+            $vehicles, 
+            $page, 
+            5
         );
-        return $this->json($vehicles);
+
+        if(!count($response)) return $this->json('Cannot find vecicles', 404);
+
+        return $this->json($response, 200, [],['groups' => ['booking_read']]);
+    }
+
+    /**
+     * @Route("/bookings/{id}", name="app_bookings_getOne", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    function findOne(int $id): Response
+    {
+        $vehicle = $this->repository->find($id);
+
+        if(!$vehicle) return $this->json('Cannot find booking with this id',404);
+
+        return $this->json($vehicle, 200, []);
     }
 
     /**
